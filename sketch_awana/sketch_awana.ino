c35f14d94a5cadc2bc298041f7b2493f445ed638
David Trotz <david.trotz@gmail.com>
2018-01-15 17:24:10 -0800

    Misc commits
    The track is built and wired and now testing and implementing

--- sketch_awana/sketch_awana.ino
+++ sketch_awana/sketch_awana.ino
@@ -14,8 +14,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 void start_00_timing();
 void mid_01_timing();
-void mid_02_timing();
-void end_03_timing();
+void end_02_timing();
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -27,33 +26,61 @@ RTC_DS1307 RTC;
 const int BREAK_SENSOR_00_INTERRUPT_PIN = 18;
 const int BREAK_SENSOR_01_INTERRUPT_PIN = 19;
 const int BREAK_SENSOR_02_INTERRUPT_PIN = 2;
-const int BREAK_SENSOR_03_INTERRUPT_PIN = 3;
 
 const int SD_CARD_CHIP_SELECT           = 53;
+
+const int YEL_LED_PIN                   = 22;
+const int BTN_01_PIN                    = 23;
+const int BTN_02_PIN                    = 24;
+const int BTN_03_PIN                    = 25;
+const int WHT_LED_PIN                   = 26;
+const int GRN_LED_PIN                   = 27;
+const int RED_LED_PIN                   = 28 ;
 
 const char CAR_OWNERS[4][16] = {"ABBY", "BEN", "WILLIAM", "JAMES"}; 
 
 // This board can be run in 8 bit or 4 bit modes. 
 // The bits indicate the width of the datapipe. 
 // 8-bit LCD (we want the added performance of 8 bits)
-LiquidCrystal lcd(12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2);
+LiquidCrystal lcd(12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 14);
 
-int backLight   = 13;
+int LCD_BACKLIGHT_PIN   = 13;
 long iter       = 0;
-char dateBuffer[256];
+char dataFilename[256];
 char timeBuffer[256];
+char floatBuffer[16];
 
 volatile signed long start_00_millis    = -1;
 volatile signed long mid_01_millis      = -1;
-volatile signed long mid_02_millis      = -1;
-volatile signed long end_03_millis      = -1;
+volatile signed long end_02_millis      = -1;
+
+bool handledStart = false;
+bool handledMid = false;
+bool handledEnd = false;
+
+int sensorState = 0, lastState=0; 
 
 void setup()
 {
+  digitalWrite(BREAK_SENSOR_00_INTERRUPT_PIN, HIGH);
   attachInterrupt(digitalPinToInterrupt(BREAK_SENSOR_00_INTERRUPT_PIN), start_00_timing, FALLING);
+  digitalWrite(BREAK_SENSOR_01_INTERRUPT_PIN, HIGH);
   attachInterrupt(digitalPinToInterrupt(BREAK_SENSOR_01_INTERRUPT_PIN), mid_01_timing, FALLING);
-  attachInterrupt(digitalPinToInterrupt(BREAK_SENSOR_02_INTERRUPT_PIN), mid_02_timing, FALLING);
-  attachInterrupt(digitalPinToInterrupt(BREAK_SENSOR_03_INTERRUPT_PIN), end_03_timing, FALLING);
+  digitalWrite(BREAK_SENSOR_02_INTERRUPT_PIN, HIGH);
+  attachInterrupt(digitalPinToInterrupt(BREAK_SENSOR_02_INTERRUPT_PIN), end_02_timing, FALLING);
+
+  pinMode(RED_LED_PIN, OUTPUT);
+  digitalWrite(RED_LED_PIN, LOW);
+  pinMode(GRN_LED_PIN, OUTPUT);
+  digitalWrite(GRN_LED_PIN, LOW);
+  pinMode(YEL_LED_PIN, OUTPUT);
+  digitalWrite(YEL_LED_PIN, LOW);
+  pinMode(WHT_LED_PIN, OUTPUT);
+  digitalWrite(WHT_LED_PIN, LOW);
+
+  pinMode(BTN_01_PIN, INPUT);
+  pinMode(BTN_02_PIN, INPUT);
+  pinMode(BTN_03_PIN, INPUT);
   
   Serial.begin(9600);
   Wire.begin();
@@ -66,8 +93,8 @@ void setup()
     Serial.println("RTC IS running!");
   }
   
-  pinMode(backLight, OUTPUT);
-  digitalWrite(backLight, HIGH); // turn backlight on. Replace 'HIGH' with 'LOW' to turn it off.
+  pinMode(LCD_BACKLIGHT_PIN, OUTPUT);
+  digitalWrite(LCD_BACKLIGHT_PIN, HIGH); // turn LCD_BACKLIGHT_PIN on. Replace 'HIGH' with 'LOW' to turn it off.
   lcd.begin(16,2);              // columns, rows.  use 16,2 for a 16x2 LCD, etc.
   lcd.clear();
 
@@ -78,27 +105,68 @@ void setup()
     while (1);
   }
   Serial.println("card initialized.");
+  DateTime now = RTC.now();
+  sprintf(dataFilename, "%02d-%02d-%02d.csv", now.year() % 2000, now.month(), now.day());
 
+  lcd.clear();
+  lcd.setCursor(0,1);
+  lcd.print("TEST");
 }
 
 void loop() {
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("");
-
-  if(iter++ < 10) {
-      File dataFile = SD.open("datalog.txt", FILE_WRITE);
-      if (dataFile) {
-        DateTime now = RTC.now(); 
-        sprintf(timeBuffer, "%02d:%02d:%02d,%d", now.hour(), now.minute(), now.second(),millis());
-        dataFile.println(timeBuffer);
-        dataFile.close();
-        lcd.setCursor(0,1);
-        lcd.print(timeBuffer);
-      }
+  digitalWrite(YEL_LED_PIN, HIGH);
+  
+  if(!handledStart && start_00_millis > 0) {
+    lcd.setCursor(9,0);
+    lcd.print("RUNNING");
+    File dataFile = SD.open(dataFilename, FILE_WRITE);
+    if (dataFile) {
+      DateTime now = RTC.now(); 
+      sprintf(timeBuffer, "%02d-%02d-%02d,%ld,", now.year(), now.month(), now.day(), start_00_millis);
+      dataFile.print(timeBuffer);
+      dataFile.close();
+    }
+    handledStart = true;
+    digitalWrite(WHT_LED_PIN, HIGH);
   }
 
-  delay(1000);
+  if(!handledMid && mid_01_millis > 0) {
+    float seconds = ((((float)mid_01_millis) - ((float)start_00_millis))/(1000.0f));
+    
+    lcd.setCursor(0,1);
+    sprintf(timeBuffer, "M:%s", dtostrf(seconds, 4, 3, floatBuffer));
+    lcd.print(timeBuffer);
+    Serial.print(timeBuffer);
+      File dataFile = SD.open(dataFilename, FILE_WRITE);
+      if (dataFile) {
+        DateTime now = RTC.now(); 
+        sprintf(timeBuffer, "%ld,%s,", mid_01_millis, dtostrf(seconds, 4, 3, floatBuffer));
+        dataFile.print(timeBuffer);
+        dataFile.close();
+      }
+      handledMid = true;
+      digitalWrite(WHT_LED_PIN, LOW);
+      digitalWrite(GRN_LED_PIN, HIGH);
+  }
+
+    if(!handledEnd && end_02_millis > 0) {
+      float seconds = ((((float)end_02_millis) - ((float)start_00_millis))/(1000.0f));
+      lcd.setCursor(9,1);
+      sprintf(timeBuffer, "E:%s", dtostrf(seconds, 4, 3, floatBuffer));
+      Serial.print(timeBuffer);
+      lcd.print(timeBuffer);
+        File dataFile = SD.open(dataFilename, FILE_WRITE);
+        if (dataFile) {
+          DateTime now = RTC.now(); 
+          sprintf(timeBuffer, "%ld,%s", end_02_millis, dtostrf(seconds, 4, 3, floatBuffer));
+          dataFile.println(timeBuffer);
+          dataFile.close();
+        }
+        handledEnd = true;
+        digitalWrite(GRN_LED_PIN, LOW);
+        digitalWrite(RED_LED_PIN, HIGH);
+  }
+
 //  if(++iter % 5000 == 0) {
 //    DateTime now = RTC.now(); 
 //
@@ -121,11 +189,7 @@ void mid_01_timing() {
     mid_01_millis = millis();
 }
 
-void mid_02_timing() {
-    mid_02_millis = millis();
-}
-
-void end_03_timing() {
-    end_03_millis = millis();
+void end_02_timing() {
+    end_02_millis = millis();
 }
 
